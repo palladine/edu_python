20975de622bac779b6195e0d5077f5c1498d07c2
Maksim <duroff.83@yandex.ru>
2019-03-14 11:18:27 +0700

    # 14.03.2019

--- descriptors.py
+++ descriptors.py
@@ -7,3 +7,25 @@ class Base:
     
 x = Base()
 print(x.attr)
+
+
+
+##
+class SuperDesc:
+
+    def __get__(self, instance, owner):
+        return instance._num
+
+    def __set__(self, instance, val):
+        instance._num = val
+
+       
+class Base:
+    def __init__(self, num):
+        self._num = num
+    num = SuperDesc()
+
+x = Base(34)
+print(x.num)
+x.num = 76
+print(x.num)
