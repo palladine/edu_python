0f0a29e8374a01b568bcb09cee75e0c0dcdf6cf1
Maksim <duroff.83@yandex.ru>
2019-03-14 10:39:59 +0700

    # 14.03.19

--- property.py
+++ property.py
@@ -8,7 +8,7 @@ class Base():
     def setage(self, val):
         self._age = val
     
-    #              getter  setter  delete doc
+    #              getter  setter deleter doc
     age = property(getage, setage, None, None)
 
 x = Base()
@@ -16,3 +16,24 @@ print(x.age)   # None
 x.age = 43
 print(x.age)   # 43
 
+
+# with decorators
+class Base(object):
+
+        def __init__(self):
+            self._x = None
+
+        x = property()
+
+        @x.getter
+        def x(self):
+            """Это свойство x."""
+            return self._x
+
+        @x.setter
+        def x(self, value):
+            self._x = value
+
+        @x.deleter
+        def x(self):
+            self._x = 'No more'
