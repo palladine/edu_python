0183c5302897674c8172914ca22bbaecdda8d745
Maksim <duroff.83@yandex.ru>
2019-03-19 12:35:46 +0700

    # 19.03.19

--- decorators.py
+++ decorators.py
@@ -83,3 +83,49 @@ func(4, 5, 6)
 func2(3, 3)
 func2(5, 5)
 #############################################
+
+##### Декоратор и к функциям и к методам классов ############
+### С помощью вложенной функции ####
+def base(func):
+    calls = 0
+    def callfunc(*args, **kwargs):
+        nonlocal calls
+        calls += 1
+        print('calls - {}, function/method - {}'.format(calls, func.__name__))
+        return func(*args, **kwargs)
+    return callfunc
+
+# к функциям
+@base
+def func(a, b, c):
+    print(a + b + c)
+
+@base
+def func2(a, b):
+    print(a * b)
+
+# к методам
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    @base
+    def getname(self):
+        return self.name
+
+    @base
+    def getmulname(self, num):
+        return self.name * num
+
+# вывод
+func(3, 4, 5)
+func(5, 6, 7)
+func2(7, 8)
+func2(8, 9)
+
+jack = Person('Jack')
+print(jack.getname())
+print(jack.getname())
+print(jack.getmulname(3))
+print(jack.getmulname(4))
+########################################################
